609c7ed0ef7f115915e689f71e44985e301a5b03
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-03-25 22:51:51 +0100

    google-api-python-client added

--- gitmostwanted/worker.py
+++ gitmostwanted/worker.py
@@ -1,5 +1,8 @@
 # pylint: disable=E1002
+from oauth2client.client import SignedJwtAssertionCredentials
 from gitmostwanted.app import app, celery
+from apiclient.discovery import build
+from httplib2 import Http
 
 
 class ContextTask(celery.Task):
@@ -12,6 +15,20 @@ class ContextTask(celery.Task):
 celery.Task = ContextTask
 
 
+def service_bigquery():
+    private_key = ''
+    email = ''
+    url = 'https://www.googleapis.com/auth/bigquery.readonly'
+
+    with open(private_key, 'rb') as f:
+        private_key = f.read()
+
+    http_auth = SignedJwtAssertionCredentials(email, private_key, url).authorize(Http())
+
+    return build('bigquery', 'v2', http=http_auth)
+
+
 @celery.task()
 def mock():
+    print(service_bigquery().datasets().list(projectId='').execute())
     pass
