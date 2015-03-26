44885f79a6fcd84a3d0a6285c3396a424a43b381
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-03-26 21:25:44 +0100

    most_starred_today drafts

---
+++ gitmostwanted/bigdata/result.py
@@ -0,0 +1,3 @@
+class Result():
+    def __init__(self, ss):
+        pass
--- gitmostwanted/worker.py
+++ gitmostwanted/worker.py
@@ -1,9 +1,10 @@
 # pylint: disable=E1002
 from oauth2client.client import SignedJwtAssertionCredentials
+from gitmostwanted.bigdata.result import Result
 from gitmostwanted.app import app, celery
-from apiclient.discovery import build
+from apiclient import discovery, errors
+from datetime import date
 from httplib2 import Http
-
 
 class ContextTask(celery.Task):
     abstract = True
@@ -22,14 +23,28 @@ def service_bigquery():
         private_key = f.read()
 
     http_auth = SignedJwtAssertionCredentials(config['email'], private_key, config['url'])
-    return build(config['service_name'], config['version'], http=http_auth.authorize(Http()))
-
-# query_request = service_bigquery().jobs()
-# query_data = {'query': 'SELECT * FROM [githubarchive:month.201503] WHERE repo_id = 28922883;'}
-# query_response = query_request.query(projectId=app.config['GOOGLE_BIGQUERY']['project_id'],
-# body=query_data).execute()
+    return discovery.build(config['service_name'], config['version'], http=http_auth.authorize(Http()))
 
 
 @celery.task()
-def mock():
-    pass
+def most_starred_today(self):
+    query_request = service_bigquery().jobs()
+    query_data = {
+        'query': """
+            SELECT
+                repo.id, repo.name, COUNT(1) AS cnt
+            FROM [githubarchive:day.events_%s]
+            WHERE type = 'WatchEvent'
+            GROUP BY repo.id, repo.name
+            ORDER BY cnt DESC
+            LIMIT 50
+        """ % date.today().strftime('%Y%m%d')
+    }
+    try:
+        query_response = query_request.query(
+            projectId=app.config['GOOGLE_BIGQUERY']['project_id'], body=query_data
+        ).execute()
+
+        result = Result(query_response)
+    except errors.HttpError as e:
+        raise self.retry(exc=e)
