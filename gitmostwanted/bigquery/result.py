a8c753dcc70ddce550f96b9f6b753e0ebc124c1b
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-03-29 13:03:51 +0200

    cleanup

--- gitmostwanted/bigquery/result.py
+++ gitmostwanted/bigquery/result.py
@@ -3,19 +3,18 @@ from collections import Iterator, Sized
 
 # @todo! add fields names from schema
 class Result(Iterator, Sized):
-    __total_rows = 0
-    __rows = []
-    __iter = 0
+    """Converts result of google-api-python-client"""
 
     def __init__(self, obj):
-        self.__total_rows = int(obj['totalRows'])
-        self.__rows = list(map(lambda x: x['f'], obj['rows']))
+        self.iter = 0
+        self.total_rows = int(obj['totalRows'])
+        self.rows = list(map(lambda x: x['f'], obj['rows']))
 
     def __next__(self):
-        if self.__iter >= self.__total_rows:
+        if self.iter >= self.total_rows:
             raise StopIteration()
-        v, self.__iter = list(map(lambda x: x['v'], self.__rows[self.__iter])), self.__iter + 1
+        v, self.iter = list(map(lambda x: x['v'], self.rows[self.iter])), self.iter + 1
         return v
 
     def __len__(self):
-        return self.__total_rows
+        return self.total_rows
