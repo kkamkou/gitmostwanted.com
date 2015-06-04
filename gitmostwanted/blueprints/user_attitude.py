7903bc0f16aa947f9f44e3a00991745874a0ab55
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-06-04 14:32:34 +0200

    typos

--- gitmostwanted/blueprints/user_attitude.py
+++ gitmostwanted/blueprints/user_attitude.py
@@ -13,17 +13,14 @@ def verify_user():
     if not g.user:
         return abort(403)
 
-def paginate(filter_by, page):
-    return Repo.query\
-        .filter(filter_by)\
-        .outerjoin(
-            UserAttitude,
-            db.and_(
-                UserAttitude.user_id == g.user.id,
-                UserAttitude.repo_id == Repo.id
-            )
-        )\
-        .paginate(page if page > 0 else 1, per_page=20, error_out=False)
+def query(filter_by):
+    return Repo.query.filter(filter_by).outerjoin(
+        UserAttitude,
+        db.and_(
+            UserAttitude.user_id == g.user.id,
+            UserAttitude.repo_id == Repo.id
+        )
+    )
 
 @user_attitude.route('/attitude/<int:repo_id>/<attitude>')
 def change(repo_id, attitude):
@@ -38,16 +35,16 @@ def change(repo_id, attitude):
 
     return make_response(render_template_string('Ok'), 204)
 
-
 @user_attitude.route('/unchecked/', defaults={'page': 1})
 @user_attitude.route('/unchecked/<int:page>')
 def unchecked(page):
-    entries = paginate(UserAttitude.repo_id.is_(None), page)
+    entries = query(UserAttitude.repo_id.is_(None)) \
+        .add_columns(db.null())\
+        .paginate(page if page > 0 else 1, per_page=20, error_out=False)
     if entries.pages < entries.page:
         return unchecked(entries.pages)
 
     return render_template('attitude.html', repos=entries)
-
 
 @user_attitude.route('/attitude/<attitude>', defaults={'page': 1})
 @user_attitude.route('/attitude/<attitude>/<int:page>')
@@ -55,8 +52,10 @@ def list_by_attitude(attitude, page):
     if not verify_attitude(attitude):
         return abort(403)
 
-    entries = paginate(UserAttitude.attitude == attitude, page)
+    entries = query(UserAttitude.attitude == attitude) \
+        .add_columns(UserAttitude.attitude)\
+        .paginate(page if page > 0 else 1, per_page=20, error_out=False)
     if entries.pages < entries.page:
-        return list(attitude, entries.pages)
+        return list_by_attitude(attitude, entries.pages)
 
     return render_template('attitude.html', repos=entries, attitude=attitude)
