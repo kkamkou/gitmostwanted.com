a78e6579ab703b9318fabaef177cc06463f2f845
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-07-22 14:40:53 +0200

    bigquery issue with an empty result

--- tests/unit/bigquery/test_result.py
+++ tests/unit/bigquery/test_result.py
@@ -4,19 +4,27 @@ from gitmostwanted.bigquery.result import ResultJob
 
 class BigQueryResultTestCase(TestCase):
     def setUp(self):
-        self.result = ResultJob(self.response_example())
+        pass
 
     def test_convert_incoming_obj(self):
-        self.assertEquals(len(self.result), 2)
-        self.assertEquals(
-            next(self.result),
-            ['29028775', 'facebook/react-native', '225']
-        )
-        self.assertEquals(
-            next(self.result),
-            ['29028776', 'facebook/react-native2', '226']
-        )
-        self.assertRaises(StopIteration, next, self.result)
+        result = ResultJob(self.response_example())
+
+        self.assertEquals(len(result), 2)
+        self.assertEquals(next(result), ['29028775', 'facebook/react-native', '225'])
+        self.assertEquals(next(result), ['29028776', 'facebook/react-native2', '226'])
+        self.assertRaises(StopIteration, next, result)
+
+    def test_convert_incoming_empty_obj(self):
+        result = ResultJob(self.response_example_empty())
+
+        self.assertEquals(len(result), 0)
+        self.assertRaises(StopIteration, next, result)
+
+    def response_example_empty(self):
+        data = self.response_example()
+        data['rows'] = []
+        data['totalRows'] = 0
+        return data
 
     def response_example(self):
         return {
