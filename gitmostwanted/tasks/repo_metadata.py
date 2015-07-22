e76d921e8036d98b86f6bab3bf760661532686ae
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-07-22 14:59:47 +0200

    typo

--- gitmostwanted/tasks/repo_metadata.py
+++ gitmostwanted/tasks/repo_metadata.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 @celery.task()
 def repo_flag_mature(num_months):
     repos = Repo.query\
-        .filter(Repo.created_at >= datetime.now() + timedelta(days=num_months * 30 * -1))\
+        .filter(Repo.created_at <= datetime.now() + timedelta(days=num_months * 30 * -1))\
         .filter(Repo.mature.is_(False))
     for repo in repos:
         repo.mature = True
