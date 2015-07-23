71632d2d0718b45e83181c9f69e744ce88b86f01
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-07-23 14:28:38 +0200

    Add a new column: status_updated_at, closes #75

---
+++ migration/versions/4610d5e2ddb_status_updated_at_column_for_repos.py
@@ -0,0 +1,26 @@
+"""status_updated_at column for repos
+
+Revision ID: 4610d5e2ddb
+Revises: 535f62f51e1
+Create Date: 2015-07-23 12:06:05.176043
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4610d5e2ddb'
+down_revision = '535f62f51e1'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('repos', sa.Column('status_updated_at', sa.DateTime(), nullable=True))
+    op.create_index(op.f('ix_repos_created_at'), 'repos', ['created_at'], unique=False)
+
+
+def downgrade():
+    op.drop_index(op.f('ix_repos_created_at'), table_name='repos')
+    op.drop_column('repos', 'status_updated_at')
