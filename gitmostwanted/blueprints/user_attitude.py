62732372ba2d5719c797e4c51b74603e59eba436
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-10-14 18:31:28 +0200

    attitude.html is moved to the "repository" folder, refs #102

--- gitmostwanted/blueprints/user_attitude.py
+++ gitmostwanted/blueprints/user_attitude.py
@@ -62,4 +62,4 @@ def list_by_attitude(attitude, page):
     if entries.pages and entries.pages < entries.page:
         return list_by_attitude(attitude, entries.pages)
 
-    return render_template('attitude.html', repos=entries, attitude=attitude, languages=languages)
+    return render_template('repository/attitude.html', repos=entries, attitude=attitude, languages=languages)
