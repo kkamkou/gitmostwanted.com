3bab2583164a1b22070d582db9f42a5e47c92f0f
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-10-20 23:05:58 +0200

    mixin.py is removed, the filtering is moved back, closes #103

--- gitmostwanted/blueprints/repo_trending.py
+++ gitmostwanted/blueprints/repo_trending.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, g, request, render_template
-from gitmostwanted.blueprints.mixin import repository_filtered
 from gitmostwanted.models.repo import Repo
 from gitmostwanted.models.user import UserAttitude
 from gitmostwanted.models import report
@@ -12,24 +11,32 @@ repo_trending = Blueprint('repo_trending', __name__)
 @repo_trending.route('/trending/<rng>/')
 def list_by_range(rng):
     map_list = {'day': 'ReportAllDaily', 'week': 'ReportAllWeekly', 'month': 'ReportAllMonthly'}
-    if rng not in map_list:
-        rng = 'day'
+    model = getattr(report, map_list.get(rng, map_list.get('day')))
 
-    model = getattr(report, map_list[rng])
-    lngs = Repo.language_distinct()
-
+    query = model.query.join(Repo).order_by(model.cnt_watch.desc())
     if not g.user:
-        query = model.query.join(Repo).add_columns(db.null())
-    else:
-        query = model.query\
-            .join(Repo)\
-            .add_columns(UserAttitude.attitude)\
-            .outerjoin(
-                UserAttitude,
-                (UserAttitude.user_id == g.user.id) & (UserAttitude.repo_id == Repo.id)
-            )
+        return list_by_range_filtered(query.add_columns(db.null()))
 
-    query = repository_filtered(request, lngs, Repo, query)\
-        .order_by(model.cnt_watch.desc())
+    return list_by_range_filtered(
+        query.add_columns(UserAttitude.attitude).outerjoin(
+            UserAttitude,
+            (UserAttitude.user_id == g.user.id) & (UserAttitude.repo_id == Repo.id)
+        )
+    )
 
-    return render_template('index.html', entries=query, languages=lngs)
+
+def list_by_range_filtered(query):
+    langs = Repo.language_distinct()
+
+    lang = request.args.get('lang')
+    if lang != 'All' and (lang,) in langs:
+        query = query.filter(Repo.language == lang)
+
+    status = request.args.get('status')
+    if status in ('promising', 'hopeless'):
+        query = query.filter(Repo.status == status)
+
+    if bool(request.args.get('mature')):
+        query = query.filter(Repo.mature.is_(True))
+
+    return render_template('index.html', entries=query, languages=langs)
