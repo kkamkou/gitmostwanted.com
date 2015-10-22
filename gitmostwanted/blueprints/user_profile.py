aa65e728ed696f7b13769a76d52be2bcd0b58c7e
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-10-22 17:53:48 +0200

    drafts, refs #23

--- gitmostwanted/blueprints/user_profile.py
+++ gitmostwanted/blueprints/user_profile.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, g, render_template, make_response, render_template_string, session
 from gitmostwanted.models.user import User, UserAttitude
 from gitmostwanted.models.repo import Repo
-
+from gitmostwanted.lib.github.api import user_starred, user_starred_star
 user_profile = Blueprint('user_profile', __name__)
 
 
@@ -13,3 +13,20 @@ def overview(name):
         .filter(UserAttitude.attitude == 'like')\
         .order_by(Repo.language.desc())
     return render_template('profile.html', account=entity, repos=repos)
+
+
+@user_profile.route('/profile/github/sync')
+def github_sync():
+    starred, code = user_starred()
+    if starred:
+        attitudes = UserAttitude.query\
+            .filter(UserAttitude.user == g.user)\
+            .filter(UserAttitude.attitude == 'like')
+
+        token, secret = session.get('github_token')
+
+        [
+            user_starred_star(r.repo.full_name, token) for r in attitudes
+            if filter(lambda x: x['full_name'] == r.repo.full_name, starred)
+        ]
+    return make_response(render_template_string('Ok'), 204)
