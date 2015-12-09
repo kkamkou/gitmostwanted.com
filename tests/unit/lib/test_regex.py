bdb5d95d11fab9cd5264ebf9dbead5b07aca469d
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-12-10 00:07:25 +0100

    SearchTest is covered, refs #79

---
+++ tests/unit/lib/test_regex.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from gitmostwanted.lib.regex import SearchTerm
+
+
+class LibRegexSearchTermTestCase(TestCase):
+    def test_detect_valid_input(self):
+        self.assertRaises(ValueError, SearchTerm, '#')
+        self.assertRaises(ValueError, SearchTerm, 'te')
+        self.assertEquals('%test%', str(SearchTerm('test')))
