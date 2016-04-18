a5a29f67b1265901746dd43766947e641897997e
Kanstantsin Kamkou <kkamkou@gmail.com>
2016-04-18 23:14:46 +0200

    2.0k should be 2k

--- gitmostwanted/lib/filter.py
+++ gitmostwanted/lib/filter.py
@@ -2,4 +2,4 @@ import math
 
 
 def number_humanize(n):
-    return n if n < 1000 else '{}k'.format(round(n / 1000, 1))
+    return n if n < 1000 else '{}k'.format(round(n / 1000, 1)).replace('.0', '')
