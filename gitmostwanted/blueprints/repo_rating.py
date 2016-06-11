a0cb98fbe5da02a41d7413690f0cec4ac34e6d03
Kanstantsin Kamkou <kkamkou@gmail.com>
2016-06-11 10:56:23 +0200

    Add "By stars" to the top section, closes #156

--- gitmostwanted/blueprints/repo_rating.py
+++ gitmostwanted/blueprints/repo_rating.py
@@ -6,24 +6,30 @@ from gitmostwanted.models.repo import Repo
 repo_rating = Blueprint('repo_rating', __name__)
 
 
-@repo_rating.route('/top/', defaults={'page': 1})
-@repo_rating.route('/top/<int:page>')
-def top(page):
-    q = Repo.filter_by_args(Repo.query, request.args)\
-        .filter(Repo.worth > 6)\
-        .order_by(Repo.worth.desc())\
-        .order_by(Repo.stargazers_count.desc())
+@repo_rating.route('/top/', defaults={'page': 1, 'sort_by': 'wanted'})
+@repo_rating.route('/top/<sort_by>/', defaults={'page': 1})
+@repo_rating.route('/top/<sort_by>/<int:page>')
+def top(sort_by, page):
+    query = Repo.filter_by_args(Repo.query, request.args).filter(Repo.worth > 6)
+    sorts = {
+        'wanted': [Repo.worth.desc(), Repo.stargazers_count.desc()],
+        'stars': [Repo.stargazers_count.desc()]
+    }
+
+    for f in [sort for sort in (sorts['wanted'] if sort_by not in sorts else sorts[sort_by])]:
+        query = query.order_by(f)
 
     if not g.user:
-        q = q.add_columns(db.null())
+        query = query.add_columns(db.null())
     else:
-        q = UserAttitude.join_by_user_and_repo(q, g.user.id, Repo.id)\
+        query = UserAttitude.join_by_user_and_repo(query, g.user.id, Repo.id)\
             .add_columns(UserAttitude.attitude)
 
-    entries = q.paginate(page if page > 0 else 1, per_page=20, error_out=False)
+    entries = query.paginate(page if page > 0 else 1, per_page=20, error_out=False)
     if entries.pages and entries.pages < entries.page:
-        return top(entries.pages)
+        return top(sort_by, entries.pages)
 
     return render_template(
-        'repository/top.html', repos=entries, page=page, languages=Repo.language_distinct()
+        'repository/top.html', languages=Repo.language_distinct(),
+        repos=entries, page=page, sort_by=sort_by
     )
