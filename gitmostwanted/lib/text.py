803c7858d60e54ec8c9e6c2adbb420dc76afa912
Kanstantsin Kamkou <kkamkou@gmail.com>
2016-10-13 21:20:13 +0200

    utf8mb4 encoding is the default from now

--- gitmostwanted/lib/text.py
+++ gitmostwanted/lib/text.py
@@ -9,12 +9,12 @@ class TextWithoutSmilies:
         self.__text = text
 
     def __str__(self):
-        return re.sub(self.pattern, ' ', self.__text).strip()
+        return re.sub(self.pattern, ' ', self.__text)
 
 
 class TextNormalized:
     def __init__(self, text: str):
-        self.__text = unicodedata.normalize('NFC', text)
+        self.__text = unicodedata.normalize('NFC', text.strip())
 
     def __str__(self):
         return self.__text.encode(errors='ignore').decode()
--- gitmostwanted/models/repo.py
+++ gitmostwanted/models/repo.py
@@ -11,11 +11,6 @@ from datetime import datetime
 
 class Repo(db.Model):
     __tablename__ = 'repos'
-    __table_args__ = {
-        'mysql_engine': 'InnoDB',
-        'mysql_charset': 'utf8',
-        'mysql_collate': 'utf8_general_ci'
-    }
 
     id = db.Column(db.BigInteger, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
