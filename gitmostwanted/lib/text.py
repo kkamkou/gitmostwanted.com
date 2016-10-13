2122811e50b01f90a01cc481eb50bcbf3dc931a4
Kanstantsin Kamkou <kkamkou@gmail.com>
2016-10-13 21:23:22 +0200

    typo

--- gitmostwanted/lib/text.py
+++ gitmostwanted/lib/text.py
@@ -9,12 +9,12 @@ class TextWithoutSmilies:
         self.__text = text
 
     def __str__(self):
-        return re.sub(self.pattern, ' ', self.__text)
+        return re.sub(self.pattern, ' ', self.__text).strip()
 
 
 class TextNormalized:
     def __init__(self, text: str):
-        self.__text = unicodedata.normalize('NFC', text.strip())
+        self.__text = unicodedata.normalize('NFC', text)
 
     def __str__(self):
         return self.__text.encode(errors='ignore').decode()
